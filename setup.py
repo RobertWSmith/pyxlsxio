8116ee0289acc62564562bf4417c66ff962e62c6
Robert Smith <robert.smith@pnc.com>
2018-06-05 09:27:49 -0400

    debugging reader

--- setup.py
+++ setup.py
@@ -7,51 +7,37 @@ import subprocess
 from Cython.Build import cythonize
 from setuptools import Extension, setup
 
-# if 'PYXLSXIO_FULL_CLEAN' in os.environ:
-#     subprocess.call(['bash', 'scripts/full-clean.sh'])
-
 retcode = subprocess.call(['bash', 'scripts/build-xlsxio.sh'])
-
 if retcode != 0:
     sys.exit(retcode)
 
 PYXLSXIO_INCLUDE_DIR = "include"
 PYXLSXIO_LIB_DIR = "lib"
 
+ext_files = glob.glob("pyxlsxio/*.pyx")
+ext_files.remove("pyxlsxio/writer.pyx")
+
 extensions = [
     Extension(
         "pyxlsxio.pyxlsxio",
         ["pyxlsxio/pyxlsxio.pyx", ],
-        extra_compile_args=['-O2', '-Wall'],
+        extra_compile_args=['-g', '-O2', '-Wall'],
         extra_link_args=['-g', ],
-        include_dirs=["."],
-        library_dirs=['.'],
     ),
     Extension(
         "pyxlsxio.reader",
         ["pyxlsxio/reader.pyx", ],
-        extra_compile_args=['-O2', '-Wall'],
+        extra_compile_args=['-g', '-O2', '-Wall'],
         extra_link_args=['-g', ],
         include_dirs=[PYXLSXIO_INCLUDE_DIR, "."],
         libraries=["xlsxio_read", "expat", "zip"],
         library_dirs=[PYXLSXIO_LIB_DIR, '.'],
     ),
-#    Extension(
-#        "pyxlsxio.writer",
-#        ["pyxlsxio/writer.pyx", ],
-#        extra_compile_args=['-O2', '-Wall'],
-#        extra_link_args=['-g', ],
-#        include_dirs=[PYXLSXIO_INCLUDE_DIR, "."],
-#        libraries=["xlsxio_write", "expat", "zip"],
-#        library_dirs=[PYXLSXIO_LIB_DIR, '.'],
-#    ),
     ]
 
 setup(
     name="pyxlsxio",
     version="2.0.0",
-    packages=[
-        'pyxlsxio',
-    ],
+    packages=["pyxlsxio", ],
     ext_modules=extensions
 )
