e05662dd647b38a99d51cf3a01c17f9a7b14fd73
Robert Smith <robert.smith@pnc.com>
2018-06-05 09:01:55 -0400

    updates, running on conda

--- pyxlsxio/__init__.py
+++
@@ -1,4 +0,0 @@
-from pyxlsxio.reader import Reader
-from pyxlsxio.writer import writer
-
-__all__ = ['Reader', 'Writer']
--- setup.py
+++ setup.py
@@ -18,35 +18,40 @@ if retcode != 0:
 PYXLSXIO_INCLUDE_DIR = "include"
 PYXLSXIO_LIB_DIR = "lib"
 
-def make_extension(name, path):
-    print(path, ": ", name)
-    return Extension(
-        name,
-        [
-            path,
-        ],
+extensions = [
+    Extension(
+        "pyxlsxio.pyxlsxio",
+        ["pyxlsxio/pyxlsxio.pyx", ],
+        extra_compile_args=['-O2', '-Wall'],
+        extra_link_args=['-g', ],
+        include_dirs=["."],
+        library_dirs=['.'],
+    ),
+    Extension(
+        "pyxlsxio.reader",
+        ["pyxlsxio/reader.pyx", ],
         extra_compile_args=['-O2', '-Wall'],
         extra_link_args=['-g', ],
         include_dirs=[PYXLSXIO_INCLUDE_DIR, "."],
-        libraries=["xlsxio_read", "xlsxio_write", "expat", "zip"],
+        libraries=["xlsxio_read", "expat", "zip"],
         library_dirs=[PYXLSXIO_LIB_DIR, '.'],
-    )
-
-def scandir(dir="pyxlsxio"):
-    output = []
-    for file in glob.glob(os.path.join(dir, "**.pyx")):
-        name = file.replace(os.path.sep, ".")[:-4]
-        output.append(make_extension(name, file))
-    return output
-
+    ),
+#    Extension(
+#        "pyxlsxio.writer",
+#        ["pyxlsxio/writer.pyx", ],
+#        extra_compile_args=['-O2', '-Wall'],
+#        extra_link_args=['-g', ],
+#        include_dirs=[PYXLSXIO_INCLUDE_DIR, "."],
+#        libraries=["xlsxio_write", "expat", "zip"],
+#        library_dirs=[PYXLSXIO_LIB_DIR, '.'],
+#    ),
+    ]
 
 setup(
     name="pyxlsxio",
     version="2.0.0",
     packages=[
         'pyxlsxio',
-        'pyxlsxio',
-    ],  # 'pyxlsxio.writer'
-    ext_modules=cythonize(scandir("pyxlsxio")),
-    # cmdclass = {'build_ext', build_ext}
+    ],
+    ext_modules=extensions
 )
